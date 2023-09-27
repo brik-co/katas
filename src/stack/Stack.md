30d5f4782c9bb0dc442922ae288ef86c95c69da4
Sam Hall <hallsamuel90@gmail.com>
2023-09-26 20:09:50 -0500

    add tdd stack kata

---
+++ src/stack/Stack.md
@@ -0,0 +1,30 @@
+# Stack
+
+## Overview
+
+The goal of this exercise is to use test-driven development skills and techniques to build and understand a Stack data structure.
+Remember to work iteratively, run the tests often, and refactor when things start to get messy.
+
+## Stack concept
+
+A Stack is a data structure typically used to demonstrate last-in first-out behavior. 
+
+Consider a _stack_ of plates in the kitchen.
+When the dishes are clean, the plates are collected one-by-one and placed in the cabinet. The first plate you picked up is on the bottom
+while the last one is on top. When someone gets hungry and needs a plate, the top plate is used first.
+
+Other real world applications include undo/redo functionality, symmetry testing (parenthesis algorithm problem), and back/forward buttons in the browser.
+
+## Stack functionality
+
+Here is the functionality of a Stack that needs to be implemented:
+
+`push` - adds an element to the top of the stack
+
+`pop` - removes the topmost element from the stack
+
+`isEmpty` - checks whether the stack is empty
+
+`isFull` - checks whether the stack is full
+
+`top` - displays the topmost element of the stack
---
+++ src/stack/Stack.test.ts
@@ -0,0 +1,112 @@
+import { expect, test, describe } from 'vitest'
+import { Stack, StackFullError } from './Stack';
+
+describe('Stack', () => {
+  describe('isEmpty should', () => {
+    test('return true for a new stack', () => {
+      const stack = new Stack();
+
+      expect(stack.isEmpty()).toEqual(true);
+    })
+
+     test('return false if elements have been added to the stack', () => {
+      const stack = new Stack();
+
+      stack.push('uno');
+
+      expect(stack.isEmpty()).toEqual(false);
+    })
+
+     test('return true if all elements have been removed from the stack', () => {
+      const stack = new Stack();
+
+      stack.push('uno');
+      stack.pop();
+
+      expect(stack.isEmpty()).toEqual(true);
+    })
+  })
+
+  describe('push should', () => {
+    test('add an element to the top of the stack', () => {
+      const stack = new Stack();
+
+      stack.push('hey');
+      stack.push('you');
+
+      expect(stack.top()).toEqual('you');
+    })
+
+    test('throw if the stack is full', () => {
+      const stack = new Stack(1);
+
+      stack.push('hey');
+      
+      expect(() => stack.push('you')).toThrow(StackFullError);
+    })
+  })
+
+  describe('pop should', () => {
+    test('return the top element from the stack', () => {
+      const stack = new Stack();
+
+      stack.push('this');
+      stack.push('is');
+      stack.push('stack');
+
+      expect(stack.pop()).toEqual('stack');
+    })
+
+    test('remove the top element from the stack', () => {
+      const stack = new Stack();
+
+      stack.push('this');
+      stack.push('is');
+      stack.push('stack');
+
+      stack.pop()
+
+      expect(stack.top()).toEqual('is');
+    })
+
+     test('return undefined if there is nothing in the stack', () => {
+      const stack = new Stack();
+
+      expect(stack.pop()).toEqual(undefined);
+    })
+  })
+
+  describe('top should', () => {
+    test('return the top element from the stack', () => {
+      const stack = new Stack();
+
+      stack.push('this');
+      stack.push('top');
+
+      expect(stack.pop()).toEqual('top');
+    })
+
+    test('return undefined if there is nothing in the stack', () => {
+      const stack = new Stack();
+
+      expect(stack.top()).toEqual(undefined);
+    })
+  })
+
+  describe('isFull should', () => {
+    test('return false is there is space left in the stack', () => {
+      const stack = new Stack(10);
+
+      expect(stack.isFull()).toEqual(false);
+    })
+
+     test('return true if the stack does not have any more capacity', () => {
+      const stack = new Stack(1);
+
+      stack.push('full');
+
+      expect(stack.isFull()).toEqual(true);
+    })
+  })
+});
+
