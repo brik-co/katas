c9c6dd56f8d2a2e852e2f7ae5812a0a2f74259a4
Sam Hall <hallsamuel90@gmail.com>
2023-09-26 20:10:35 -0500

    add tdd stack kata (#2)

---
+++ src/stack/Stack.ts
@@ -0,0 +1,35 @@
+export class Stack<T> {
+  private readonly dataStore: T[];
+  private readonly size: number;
+
+  constructor(size = 100) {
+    this.dataStore = [];
+    this.size = size;
+  }
+
+  public isEmpty() {
+    return this.dataStore.length === 0;
+  }
+
+  public push(element: T) {
+    if (this.isFull()) {
+      throw new StackFullError();
+    }
+
+    this.dataStore.push(element);
+  }
+
+  public pop() {
+    return this.dataStore.pop();
+  }
+
+  public top() {
+    return this.dataStore[this.dataStore.length - 1];
+  }
+
+  public isFull() {
+    return this.dataStore.length === this.size;
+  }
+}
+
+export class StackFullError extends Error { }
